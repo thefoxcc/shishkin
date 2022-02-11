826574c32010a5f029c1dacb9703ee526da5c9ee
thefoxcc <94108436+thefoxcc@users.noreply.github.com>
2022-02-11 13:12:58 +0400

    Update main.py

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ def getwiki(s):
         wikitext=ny.content[:1000]
         wikimas=wikitext.split('.')
         wikimas = wikimas[:-1]
-        wikitext2 = ''
+        wikitexts = ''
         for x in wikimas:
             if not('==' in x):
                 if(len((x.strip()))>3):
-                   wikitext2=wikitext2+x+'.'
+                   wikitexts=wikitexts+x+'.'
             else:
                 break
-        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
-        return wikitext2
+        wikitexts=re.sub('\([^()]*\)', '', wikitexts)
+        wikitexts=re.sub('\([^()]*\)', '', wikitexts)
+        wikitexts=re.sub('\{[^\{\}]*\}', '', wikitexts)
+        return wikitexts
     except Exception as e:
-        return 'В энциклопедии нет информации об этом'
+        return 'В Wikipedia нет информации об этом'
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
+    bot.send_message(m.chat.id, 'Отправьте мне слово, и я найду его на Wikipedia')
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     bot.send_message(message.chat.id, getwiki(message.text))
