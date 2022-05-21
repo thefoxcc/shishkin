2deaccc6360598ea67041e31373d907e3450eb7e
thefoxcc <94108436+thefoxcc@users.noreply.github.com>
2022-05-21 10:51:50 +0400

    Update labs6.py

--- labs6.py
+++ labs6.py
@@ -2,16 +2,14 @@ import time
 import random
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-      print (time.ctime())
-
+print (time.ctime())
 N = int(input("Введите N > 3\n"))
 while N < 4:
     N = int(input("Недопустмое значение N\nВведите N > 3\n"))
 size = N // 2
 K = int(input("Введите K\n"))
-
 start = time.time()
 
 A = np.zeros((N, N), dtype = int) #Задаём матрицы А и Ф = 0
@@ -21,21 +19,21 @@ for i in range (N): #Задаём матрицу А
     for j in range (N):
         A[i][j] = random.randint(-10,10)
 
-      print ("\nMatrix A:")
+print ("\nMatrix A:")
 for i in A:
     for j in i:
         print ("%5d" % j, end = ' ')
-    print () #
+    print () 
 
 for i in range (N): #Задаём матрицу Ф = А
     for j in range (N):
         F[i][j] = A[i][j]
 
-      print ("\nMatrix F:")
+print ("\nMatrix F:")
 for i in F:
     for j in i:
         print ("%5d" % j, end = ' ')
-      print ()
+    print ()
 
 E = np.zeros((size, size), dtype = int) #Задаём матрицу Е
 cnt = 0
@@ -49,12 +47,12 @@ for i in range (size):
         if (i+1) % 2 != 0:
             mult *= E[i][j]
 
-      print ("\nMatrix E:")
+print ("\nMatrix E:")
 for i in E:
     for j in i:
         print ("%5d" % j, end = ' ')
-      print ()
-      print ("\nСумма =", cnt, "\nПроизведение =", mult)
+    print ()
+print ("\nСумма =", cnt, "\nПроизведение =", mult)
 
 if cnt < mult:
     for i in range (size):
@@ -65,14 +63,14 @@ else:
         for j in range (size):
             F[i][j + size + N % 2], F[i + size + N % 2][j + size + N % 2] = F[i + size + N % 2][j + size + N % 2], F[i][j + size + N % 2]
 
-      print ("\nMatrix F(modified):")
+print ("\nMatrix F(modified):")
 for i in F:
     for j in i:
         print ("%5d" % j, end = ' ')
-      print ()
+    print ()
 
 if np.linalg.det(A) == 0 or np.linalg.det(F) == 0:
-      print ("Вычисления невозможны")
+    print ("Вычисления невозможны")
 
 elif np.linalg.det(A) > np.trace(F):
     Matrix = (((np.linalg.inv(A)).dot(np.transpose(A)))-(np.transpose(F) * K)) #A-1*AT – K * FТ
@@ -80,17 +78,17 @@ elif np.linalg.det(A) > np.trace(F):
 else:
     Matrix = ((A + np.tril(A) - np.linalg.det(F))*K) #(A +GТ-F-1)*K
 
-print ("\nCalculations Matrix:")
+print ("\nCalc Matrix:")
 for i in Matrix:
     for j in i:
         print ("%5d" % j, end = ' ')
-      print ()
+    print ()
 
 finish = (time.time() - start)
 print ("\n", "Time", finish, "seconds")
 
 # MATPLOTLIB:
-plt.title("Changing element of matrix rows")  # 1 пример
+plt.title("Changing element of matrix rows")  # 1 
 plt.xlabel("Matrix column number")
 plt.ylabel("Element value")
 
@@ -99,14 +97,14 @@ for j in range(N):
 
 plt.show()
 
-np.random.seed(123)                           # 2 пример
+np.random.seed(123)                           # 2 
 for j in range (N):
     plt.pcolormesh(F, cmap='plasma', edgecolors="k", shading='flat')
 plt.title("Changing element of matrix rows")
 
 plt.show()
 
-fig, ax = plt.subplots()                      # 3 пример
+fig, ax = plt.subplots()                      # 3 
 
 for j in range (N):
     ax.scatter([i for i in range(N)], F[j][::])
@@ -116,20 +114,19 @@ plt.xlabel("Matrix column number")
 plt.ylabel("Element value")
 plt.show()
 
-# SEABORN:
-sns.set_style("whitegrid")                    # 1 пример
+sns.set_style("whitegrid")                    # 1 
 sns.boxplot(data = F)
 plt.xlabel("Matrix column number")
 plt.ylabel("Element value")
 plt.show()
 
-sns.set_theme(style = "darkgrid")             # 2 пример
+sns.set_theme(style = "darkgrid")             # 2 
 plt.xlabel("Matrix column number")
 plt.ylabel("Element value")
 sns.lineplot(data = F)
 plt.show()
 
-sns.heatmap(data = F, annot = True)             # 3 пример
+sns.heatmap(data = F, annot = True)             # 3 
 plt.xlabel("Matrix column number")
 plt.ylabel("Matrix row number")
 plt.show()
