977024c88e06afb1cec0c058cdc58fa7d445e561
thefoxcc <94108436+thefoxcc@users.noreply.github.com>
2022-05-21 09:56:12 +0400

    Update labs5.py

--- labs5.py
+++ labs5.py
@@ -1,15 +1,16 @@
 import numpy as np
 import random
 import time
-      print (time.ctime())
+
+print (time.ctime())
 N = int(input("Введите N > 3\n"))
-    while N < 4:
-N = int(input("Недопустмое значение N\nВведите N > 3\n"))
-    size = N // 2
+while N < 4:
+    N = int(input("Недопустмое значение N\nВведите N > 3\n"))
+size = N // 2
 K = int(input("Введите K\n"))
 start = time.time()
-    A = np.zeros((N, N), dtype = int) #Задаём матрицы А и Ф = 0
-    F = np.zeros((N, N), dtype = int)
+A = np.zeros((N, N), dtype = int) #Задаём матрицы А и Ф = 0
+F = np.zeros((N, N), dtype = int)
 for i in range (N): #Задаём матрицу А
     for j in range (N):
         A[i][j] = random.randint(-10,10)
@@ -17,15 +18,15 @@ print ("\nMatrix A:")
 for i in A:
     for j in i:
         print ("%5d" % j, end = ' ')
-        print () #
+    print () 
 for i in range (N): #Задаём матрицу Ф = А
     for j in range (N):
         F[i][j] = A[i][j]
-      print ("\nMatrix F:")
+print ("\nMatrix F:")
 for i in F:
     for j in i:
         print ("%5d" % j, end = ' ')
-        print ()
+    print ()
 E = np.zeros((size, size), dtype = int) #Задаём матрицу Е
 cnt = 0
 mult = 1
@@ -36,12 +37,12 @@ for i in range (size):
             cnt += 1
         if (i+1) % 2 != 0:
             mult *= E[i][j]
-      print ("\nMatrix E:")
+print ("\nMatrix E:")
 for i in E:
     for j in i:
         print ("%5d" % j, end = ' ')
-      print ()
-      print ("\nСумма =", cnt, "\nПроизведение =", mult)
+    print ()
+print ("\nСумма =", cnt, "\nПроизведение =", mult)
 if cnt < mult:
     for i in range (size):
         for j in range (size):
@@ -50,7 +51,7 @@ else:
     for i in range (size):
         for j in range (size):
             F[i][j + size + N % 2], F[i + size + N % 2][j + size + N % 2] = F[i + size + N % 2][j + size + N % 2], F[i][j + size + N % 2]
-      print ("\nMatrix F(modified):")
+print ("\nMatrix F(modified):")
 for i in F:
     for j in i:
         print ("%5d" % j, end = ' ')
@@ -61,10 +62,10 @@ elif np.linalg.det(A) > np.trace(F):
     Matrix = (((np.linalg.inv(A)).dot(np.transpose(A)))-(np.transpose(F) * K)) #A-1*AT – K * FТ
 else:
     Matrix = ((A + np.tril(A) - np.linalg.det(F))*K) #(A +GТ-F-1)*K
-      print ("\nCalculations Matrix:")
+print ("\nCalculations Matrix:")
 for i in Matrix:
     for j in i:
         print ("%5d" % j, end = ' ')
     print ()
-      finish = (time.time() - start)
-      print ("\n", "Time", finish, "seconds")
+finish = (time.time() - start)
+print ("\n", "Time", finish, "seconds")
